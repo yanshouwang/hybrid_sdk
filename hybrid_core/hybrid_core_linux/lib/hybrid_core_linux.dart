44780a614b79541120c9ae4927de6a2c1e5ae457
iAMD <yanshouwang@outlook.com>
2024-03-25 12:13:59 +0800

    删除插件注册日志

--- hybrid_core/hybrid_core_linux/lib/hybrid_core_linux.dart
+++ hybrid_core/hybrid_core_linux/lib/hybrid_core_linux.dart
@@ -1,7 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 abstract class HybridCoreLinux {
-  static void registerWith() {
-    debugPrint('hybrid_core is registered.');
-  }
+  static void registerWith() {}
 }
--- hybrid_core/hybrid_core_windows/lib/hybrid_core_windows.dart
+++ hybrid_core/hybrid_core_windows/lib/hybrid_core_windows.dart
@@ -1,7 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 abstract class HybridCoreWindows {
-  static void registerWith() {
-    debugPrint('hybrid_core is registered.');
-  }
+  static void registerWith() {}
 }
