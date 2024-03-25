55612e8cd3e7b4a80ee344cc20189c45e5d88619
iAMD <yanshouwang@outlook.com>
2024-03-25 11:19:01 +0800

    创建 Windows 和 Linux 项目

---
+++ hybrid_core/hybrid_core_linux/lib/hybrid_core_linux.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart';
+
+abstract class HybridCoreLinux {
+  static void registerWith() {
+    debugPrint('hybrid_core is registered.');
+  }
+}
---
+++ hybrid_core/hybrid_core_windows/lib/hybrid_core_windows.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart';
+
+abstract class HybridCoreWindows {
+  static void registerWith() {
+    debugPrint('hybrid_core is registered.');
+  }
+}
