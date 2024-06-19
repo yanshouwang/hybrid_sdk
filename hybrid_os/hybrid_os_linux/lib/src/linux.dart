dbea86f5aba08f6d3706735ff7e561eca005c97d
iAMD <yanshouwang@outlook.com>
2024-06-19 11:45:22 +0800

    Add comments.

--- hybrid_os/hybrid_os_linux/lib/src/linux.dart
+++ hybrid_os/hybrid_os_linux/lib/src/linux.dart
@@ -1,5 +1,7 @@
 import 'package:hybrid_os_platform_interface/hybrid_os_platform_interface.dart';
 
+/// Linux Platform.
 final class LinuxPlatform extends OSPlatform implements Linux {}
 
+/// Linux OS.
 abstract interface class Linux implements OS {}
