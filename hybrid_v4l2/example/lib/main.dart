c877ba4cce3f102c06ae60637fb65a7bf0cad711
yanshouwang <yanshouwang@outlook.com>
2024-09-26 22:24:56 +0800

    Fix memory issues.

--- hybrid_v4l2/example/lib/main.dart
+++ hybrid_v4l2/example/lib/main.dart
@@ -1,17 +1,46 @@
-import 'package:flutter/foundation.dart';
+import 'dart:developer';
+
 import 'package:hybrid_v4l2/hybrid_v4l2.dart';
+import 'package:logging/logging.dart';
 
 void main() {
+  Logger.root.onRecord.listen(onLogRecord);
   final v4l2 = V4L2();
   final device = v4l2.open('/dev/video0');
-  final deviceCapability = v4l2.queryCapability(device);
-  debugPrint(
-      '''driver ${deviceCapability.driver}, card ${deviceCapability.card}, bus ${deviceCapability.busInfo}, version ${deviceCapability.version}
+  try {
+    final capability = v4l2.queryCapability(device);
+    final driver = capability.driver;
+    final card = capability.card;
+    final busInfo = capability.busInfo;
+    final version = capability.version;
+    final capabilities = capability.capabilities;
+    final deviceCapabilities = capability.deviceCapabilities;
+    Logger.root.info('''/dev/video0
 
-capabilities
-${deviceCapability.capabilities.join('\n')}
+driver: $driver
+card: $card
+busInfo: $busInfo
+version: ${(version >> 16) & 0xffff}.${(version >> 8) & 0xff}.${version & 0xff}
 
-deviceCapabilities
-${deviceCapability.deviceCapabilities.join('\n')}''');
-  v4l2.close(device);
+capabilities:
+${capabilities.join('\n')}
+
+deviceCapabilities:
+${deviceCapabilities.join('\n')}''');
+  } finally {
+    v4l2.close(device);
+  }
+}
+
+void onLogRecord(LogRecord record) {
+  log(
+    record.message,
+    time: record.time,
+    sequenceNumber: record.sequenceNumber,
+    level: record.level.value,
+    name: record.loggerName,
+    zone: record.zone,
+    error: record.error,
+    stackTrace: record.stackTrace,
+  );
 }
--- hybrid_v4l2/lib/src/v4l2_impl.dart
+++ hybrid_v4l2/lib/src/v4l2_impl.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'dart:ffi' as ffi;
 
 import 'package:ffi/ffi.dart' as ffi;
 
@@ -13,8 +13,9 @@ import 'v4l2_error.dart';
 final class V4L2Impl implements V4L2 {
   @override
   V4L2Device open(String deviceName) {
-    final fd =
-        ffi.libV4L2.open(deviceName.toNativeUtf8().cast(), ffi.O_RDWR, 0);
+    final file = deviceName.toNativeUtf8().cast<ffi.Char>();
+    final fd = ffi.libV4L2.open(file, ffi.O_RDWR, 0);
+    ffi.malloc.free(file);
     if (fd == -1) {
       throw V4L2Error('open failed, $fd.');
     }
@@ -27,7 +28,7 @@ final class V4L2Impl implements V4L2 {
       throw TypeError();
     }
     final status = ffi.libV4L2.close(device.fd);
-    if (status == -1) {
+    if (status != 0) {
       throw V4L2Error('close failed, $status.');
     }
   }
@@ -37,14 +38,13 @@ final class V4L2Impl implements V4L2 {
     if (device is! V4L2DeviceImpl) {
       throw TypeError();
     }
-    return ffi.using((arena) {
-      final cap = arena<ffi.v4l2_capability>();
-      final status = ffi.libV4L2.ioctl(device.fd, ffi.VIDIOC_QUERYCAP, cap);
-      if (status == -1) {
-        throw V4L2Error('ioctl failed, $status.');
-      }
-      return V4L2DeviceCapabilityImpl(cap.ref);
-    });
+    final capability = V4L2DeviceCapabilityImpl();
+    final status =
+        ffi.libV4L2.ioctl(device.fd, ffi.VIDIOC_QUERYCAP, capability.ffiValue);
+    if (status != 0) {
+      throw V4L2Error('ioctl failed, $status.');
+    }
+    return capability;
   }
 }
 
@@ -54,28 +54,33 @@ final class V4L2DeviceImpl implements V4L2Device {
   V4L2DeviceImpl(this.fd);
 }
 
-class V4L2DeviceCapabilityImpl implements V4L2DeviceCapability {
-  final ffi.v4l2_capability ffiValue;
+class V4L2DeviceCapabilityImpl
+    implements V4L2DeviceCapability, ffi.Finalizable {
+  static final finalizer = ffi.NativeFinalizer(ffi.malloc.nativeFree);
 
-  V4L2DeviceCapabilityImpl(this.ffiValue);
+  final ffi.Pointer<ffi.v4l2_capability> ffiValue;
 
-  @override
-  String get driver => ffiValue.driver.dartValue;
-
-  @override
-  String get card => ffiValue.card.dartValue;
-
-  @override
-  String get busInfo => ffiValue.bus_info.dartValue;
+  V4L2DeviceCapabilityImpl() : ffiValue = ffi.malloc<ffi.v4l2_capability>() {
+    finalizer.attach(this, ffiValue.cast());
+  }
 
   @override
-  int get version => ffiValue.version;
+  String get driver => ffiValue.ref.driver.dartValue;
+
+  @override
+  String get card => ffiValue.ref.card.dartValue;
+
+  @override
+  String get busInfo => ffiValue.ref.bus_info.dartValue;
+
+  @override
+  int get version => ffiValue.ref.version;
 
   @override
   List<V4L2Capability> get capabilities =>
-      ffiValue.capabilities.dartCapabilities;
+      ffiValue.ref.capabilities.dartCapabilities;
 
   @override
   List<V4L2Capability> get deviceCapabilities =>
-      ffiValue.device_caps.dartCapabilities;
+      ffiValue.ref.device_caps.dartCapabilities;
 }
